e243a428ecc7845030cb81696c29c9a0c95e0e69
BobertoBobkinsIII <50548791+BobertoBobkinsIII@users.noreply.github.com>
2020-09-19 12:02:01 -0400

    changed tests.py

--- tests.py
+++ tests.py
@@ -3,17 +3,27 @@ import numpy as np
 from tensorgrad.engine import Tensor
 
 #TODO: make this a real testing script
-xpt = torch.Tensor([1,2])
-xpt.requires_grad = True
-ypt = torch.Tensor([2,2])
-zpt = xpt*ypt
-wpt = sum(zpt)
-wpt.backward()
 
+def helper(fn,fn_input):
+    xpt = torch.Tensor(fn_input)
+    xpt.requires_grad = True
 
-x = Tensor([1,2])
-y = Tensor([2,2])
-z = x * y
-w = sum(z)
-w.backward()
+    ypt = fn(xpt)
+    ypt.backward()
 
+    x = Tensor(fn_input)
+    
+    y = fn(x)
+
+    y.backward()
+    assert np.all(y.data == ypt.detach().numpy())
+    assert np.all(x.grad == xpt.grad.detach().numpy())
+
+def test_same_shape_add():
+    fn_input = np.array([1,2,3])
+    def fn(x):
+        z = x + [4,3,2]
+        return z.sum()
+    helper(fn,fn_input)
+
+test_same_shape_add()
